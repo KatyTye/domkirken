e030facdab4ab60ce77e29e3cc174d3bae31b27b
David Majholt <d.boy.dragoz@gmail.com>
2025-12-04 09:28:40 +0100

    feat: updated footer components classes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,12 +4,12 @@ export default function Footer() {
 
 	return (
 		<footer className="bottom-content">
-			<a className="bottom-content__redirect" href="https://nmdanmarkskirkerprod.blob.core.windows.net/files/Roskilde_Domkirke_-_planer.pdf"
+			<a className="bottom-content__redirect redirect-link" href="https://nmdanmarkskirkerprod.blob.core.windows.net/files/Roskilde_Domkirke_-_planer.pdf"
 				target="_blank">Tegninger</a>
 			<a className="bottom-content__redirect" href="https://github.com" target="_blank">
 				<FaGithub className="bottom-content__redirect-icon" />
 			</a>
-			<a className="bottom-content__redirect" href="http://holsoe-ark.dk/?p=447&lang=en"
+			<a className="bottom-content__redirect redirect-link" href="http://holsoe-ark.dk/?p=447&lang=en"
 				target="_blank">Tag Konstruktion</a>
 		</footer>
 	)
