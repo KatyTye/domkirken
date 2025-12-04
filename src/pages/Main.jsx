6000df2d9517775996a881c9211ccf032505c410
Overlordka <kopary787@gmail.com>
2025-12-04 09:31:42 +0100

    feat: main side styling

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -2,7 +2,18 @@ function Main() {
 
 	return (
 		<main className="main-content">
-
+			<section className="main-content__sec-hero">
+				<img src={"./Image2.jpg"} alt="a image of something" className="main-content__img"></img>
+			</section>
+			<article className="main-content__art">
+				<h2 className="main-content__h2">Roskilde Domkirke</h2>
+				<p className="main-content__p">
+					Roskilde Domkirke er domkirken i Roskilde. Domkirken er blandt de vigtigste kirkebygninger i Danmark og et af de vigtigste monumenter overhovedet på grund af sine enestående arkitektur 
+					og som gravkirke for Danmarks kongeslægt. Domkirken er Nordens første gotiske bygningsværk og en af de første teglstenskirker overhovedet opført omkring år 1200, men rummer i dag arkitektoniske stilarter fra en periode på 800 år. 
+					Domkirken har været kongefamiliens gravkirke gennem århundreder og rummer 40 kongegrave (37 kendte og 3 tilskrevet). Sammen med kirkens kalkmalerier og kunst fortæller Roskilde Domkirke kongernes, kirkens og Danmarks historie 
+					gennem 1000 år. Domkirken blev i 1995 optaget på UNESCOs verdensarvsliste.
+				</p>
+			</article>
 		</main>
 	)
 }
